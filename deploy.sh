4aea6b44bcf156cc3988a55af120750eb7829281
Jason Rayles <jason.rayles@nbcuni.com>
2019-05-03 13:54:54 -0400

    Added deployment script

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+docker build -t jasonrayles/multi-client:latest -t jasonrayles/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t jasonrayles/multi-server:latest -t jasonrayles/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t jasonrayles/multi-worker:latest -t jasonrayles/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push jasonrayles/multi-client:latest
+docker push jasonrayles/multi-server:latest
+docker push jasonrayles/multi-worker:latest
+docker push jasonrayles/multi-client:$SHA
+docker push jasonrayles/multi-server:$SHA
+docker push jasonrayles/multi-worker:$SHA
+kubectl apply -f k8s
+kubectl set image deployments/client-deployment server=jasonrayles/multi-client:$SHA
+kubectl set image deployments/server-deployment server=jasonrayles/multi-server:$SHA
+kubectl set image deployments/worker-deployment server=jasonrayles/multi-worker:$SHA
